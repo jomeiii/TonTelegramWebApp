d909a0781c48e23a647538de0a7d0339b774e28d
ShchetkinA <97796454+ShchetkinA@users.noreply.github.com>
2024-09-23 22:58:08 +0500

    Connect to master server

---
+++ TonTelegramWebApp/Assets/Scripts/Managers/LobbyManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected");
+    }
+}
