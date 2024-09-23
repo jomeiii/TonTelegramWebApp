6d11b11efe41785430e8755b3e132648013d6c1c
jomeiii <136589834+jomeiii@users.noreply.github.com>
2024-09-23 22:48:24 +0500

    Build project

---
+++ TonTelegramWebApp/Assets/Scripts/Cliker/GameManager.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _score;
+    [SerializeField] private TextMeshProUGUI _textMeshPro;
+
+    public void ClickButttonHandler()
+    {
+        _score += 1;
+        _textMeshPro.text = $"Score: {_score}";
+    }
+}
